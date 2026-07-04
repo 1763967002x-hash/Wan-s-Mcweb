f584b1a66bcae6e2266acadc95b92598a5928b17
1763967002x-hash <1763967002x@gmail.com>
2026-07-05 07:40:12 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mc.jungles.cc.cd',
-  trailingSlash: 'never',
-  build: {
-    format: 'file'
-  },
-  integrations: [
-    sitemap({
-      exclude: ['/404']
-    })
-  ]
+  site: 'https://mc.jungles.cc',
+  integrations: []
 });
