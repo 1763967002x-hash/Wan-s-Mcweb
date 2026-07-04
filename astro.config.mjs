8711c5299fe3d5dc24c8a5104c97379f5e09d7c1
1763967002x-hash <1763967002x@gmail.com>
2026-07-05 07:20:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://minecraft-server.example.com',
-  integrations: []
+  integrations: [
+    sitemap({
+      exclude: ['/404']
+    })
+  ]
 });
